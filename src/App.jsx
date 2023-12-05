86ddd0608afdcb01b0b2bd26ec3226f33328f7dc
vijaykrishna <vijaykrishna.top@gmail.com>
2023-12-05 19:16:11 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/comman/header'
+import Footer from './components/comman/footer'
+
+function App() {
+  
+
+
+  return (
+    <>
+    <div className='app'>
+    <Header/>
+    <Footer/>
+    </div>
+    </>
+  )
+}
+
+export default App
